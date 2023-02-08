daff10b0cceb92c5d1038f22ffa3445457972265
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-07 20:10:29 -0500

    installed Mongoose. created a Post route where the form will submit user login. added middleware that parses req.body. form action redirects to post route.

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ const ejs = require("ejs");
 //Imported Models:
 const User = require("./models/user");
 
+//Imported mongoose db:
+mongoose.connect('mongodb://127.0.0.1:27017/AuthDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
+
+//Middleware:
 app.set("view engine", "ejs");
 app.set("views", "views");
-
+app.use(express.urlencoded({ extended: true }));// we need this to parse req.body
 
 
 
@@ -24,9 +35,14 @@ app.get("/secret", (req, res) => {
     res.send("Secret route! cant be seen untill logged in!")
 })
 
-
+//Get route: create a new user
 app.get("/register", (req, res) => {
     res.render("register");
+})
+
+//Post route: submit a user from form
+app.post("/register", async (req, res) => {
+    res.send(req.body);
 })
 
 // App is listening on Port:
