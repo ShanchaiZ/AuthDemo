fc30fd4c2dd36d6aaa0d5058505acf54078bd787
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-07 21:47:56 -0500

    added a login post route. The username is found in our db when they registered. then bcrypt compares the user password on input vs the hashed password in db. The login result is a T/F boolean result.

--- index.js
+++ index.js
@@ -36,10 +36,6 @@ app.get("/", (req, res) => {
     res.send("Home page!")
 })
 
-app.get("/secret", (req, res) => {
-    res.send("Secret route! cant be seen untill logged in!")
-})
-
 //Get route: create a new user
 app.get("/register", (req, res) => {
     res.render("register");
@@ -50,7 +46,7 @@ app.post("/register", async (req, res) => {
     const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
     const user = new User({
-        username,
+        username: username,
         password: hash
     });
     await user.save();
@@ -58,7 +54,31 @@ app.post("/register", async (req, res) => {
 });
 
 
+//Get Route: User Login form
+app.get('/login', (req, res) => {
+    res.render("login");
+})
 
+
+//Post Route: User Login form action:
+app.post('/login', async (req, res) => {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username: username }); //find user in our db
+
+    //once we find the user, bcrypt compares user password with hashed password which returns a boolean result.
+    const validPassword= await bcrypt.compare(password, user.password);
+    if (validPassword) {
+        res.send('WELCOME!!')
+    } else {
+        res.send("Incorrect username or password! try again!")
+    }
+
+})
+
+
+app.get("/secret", (req, res) => {
+    res.send("Secret route! cant be seen untill logged in!")
+})
 
 // App is listening on Port:
 //==================================================================
@@ -68,9 +88,13 @@ app.listen(3000, () => {
 
 
 //=====================================================================
+// Objective: is to hide the /secret route and only select few can access that route:
+
 //Take user and password = create a new user!
 //we will use "bcrypt" which will make us a new hashed password then it will be stored in our db.
 
+
+//A. REGISTERING A USER:
 //since we dont want to openly store passwords: we need to bcrypt the post route of password:
 // 1. destructure the username and password = req.body
 // 2. await bcrypt.hash(the password, saltrounds)
@@ -78,4 +102,19 @@ app.listen(3000, () => {
 // 4. user.save();
 // 5. redirect to login page
 
+//----------------------------------------
 
+//B. AUTHENTICATION:
+//login form:
+//similar to register form but form action is different. then,
+// create a post route: where we findone user with that username. once we find that user,
+// we want to compare the hashed password to the password given by user.  as per docs: const validPassword= await bcrypt.compare(password, user.password);
+//we write if statement about login sucess/failure result. 
+
+//NOTE: if the password is incorrect or there is no username with that name we want to let the user know. BUT what we dont want the user to know is username is correct but password is wrong. we dont want ppl to guess with feedback!! best practise: "incorrect username or password"
+// as of this point: we can register a user => we can log in as user BUT we cant remember user between request.
+
+
+//----------------------------------------
+
+// C. KEEPING A USER LOGGED IN (using cookies/session);
